ee027df311a93705e00a3e49d15a3300b34a546c
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-02-21 10:43:44 +0000

    BASICS

---
+++ react_basics/01.js
@@ -0,0 +1,25 @@
+// React – Simple Definition
+
+// React is a JavaScript library used to build user interfaces (UI), especially for single-page applications (SPA).
+
+// It was created by Facebook (now Meta Platforms).
+
+// 🔹 In Simple Words:
+
+// React helps you build websites where:
+
+// The page updates without reloading
+
+// UI is made using components
+
+// Code is reusable and fast
+
+// 🔹 Why React is Popular?
+
+// Uses Virtual DOM (faster updates)
+
+// Component-based structure
+
+// Easy to manage large projects
+
+// Strong community support
