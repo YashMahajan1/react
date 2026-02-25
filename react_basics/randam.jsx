76524d448490039dc740423f457837a5d649ad62
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-02-25 16:51:41 +0000

    reublity_rect_comp

---
+++ react_basics/randam.jsx
@@ -0,0 +1,25 @@
+function Random(){
+    let num = Math.random() * 10;
+return <h1 style={{"backgroundColor": "#132342"}}>randam no is {Math.round(num)}</h1>
+}
+export default Random;
+
+
+
+
+// import Random from "./randam";
+//  function App() {
+//    return  <div> 
+//     <h1>
+//     react jsx
+//    </h1>
+   
+//    <Random></Random>
+//    <Random></Random>
+//    <Random></Random>
+//    <Random></Random>
+//    <Random></Random>
+//    </div>
+   
+//  }
+//  export default App; 
