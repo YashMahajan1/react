273a06db27ae358ef284951674f799d77acc9430
YashMahajan1 <140370973+YashMahajan1@users.noreply.github.com>
2026-03-04 14:27:50 +0530

    CONDITINAL_RENDARING

--- App.jsx
+++ App.jsx
@@ -4,10 +4,12 @@ import './App.css'
 function App() {
 
   let item = ['dal','vegetables','roti','oats','fruits','milk']
-  
+  //let ternare = item.length===0 ? <h3>still hungry</h3>:null; {ternare}
+ let logical =item.length===0&& <h3>still hungry</h3>
+ 
     return <>
       <h1>healthy food</h1>
-
+    {logical}
   <ul className="list-group">
     {item.map((item)=>(<li key={item} className="list-group-item">{item}</li>))}
   
